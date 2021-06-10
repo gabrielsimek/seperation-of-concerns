88540b59a23759b2d27fb99801c2235460e9322e
Gabriel Simek <gabrielsimek@outlook.com>
2021-06-10 12:12:44 -0700

    getOrders and getOreder tests passing

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -2,6 +2,7 @@ const pool = require('../lib/utils/pool');
 const setup = require('../data/setup');
 const request = require('supertest');
 const app = require('../lib/app');
+// const TaskRouterCapability = require('twilio/lib/jwt/taskrouter/TaskRouterCapability');
 
 jest.mock('twilio', () => () => ({
   messages: {
@@ -10,7 +11,7 @@ jest.mock('twilio', () => () => ({
 }));
 
 describe('03_separation-of-concerns-demo routes', () => {
-  beforeEach(() => {
+  beforeAll(() => {
     return setup(pool);
   });
 
@@ -25,5 +26,38 @@ describe('03_separation-of-concerns-demo routes', () => {
       .send({ quantityOfItems: 10 });
 
     expect(res.body).toEqual(expectation);
+  });
+  //needs to return rows in JSON for id in num???
+  it('gets all orders from our db', async() => {
+    const item1 = {
+      id:  expect.any(String),
+      quantityOfItems: 10,
+    };
+    const item2 = {
+      id: expect.any(String),
+      quantityOfItems: 15,
+    };
+
+    await request(app)
+      .post('/api/v1/orders')
+      .send({ quantityOfItems: 15 });
+ 
+    
+    const res = await request(app)
+      .get('/api/v1/orders');
+
+    expect(res.status).toBe(200);
+    expect(res.body).toEqual(expect.arrayContaining([item1, item2]));
+  });
+  it('gets a single item from db', async() => {
+    const item1 = {
+      id: '1',
+      quantityOfItems: 10,
+    };
+
+    const res = await request(app)
+      .get(`/api/v1/orders/${item1.id}`);
+    expect(res.status).toBe(200);
+    expect(res.body).toEqual(item1);
   });
 });
